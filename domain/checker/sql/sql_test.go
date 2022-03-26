4b2f6d65c3bc9db03b3e102b7943a5b54b978202
Marcelo Campos Rocha <mcrocha@gmail.com>
2022-03-25 21:09:41 -0300

    implemented sql checker

--- domain/checker/sql/sql_test.go
+++ domain/checker/sql/sql_test.go
@@ -1,9 +1,47 @@
 package sql_test
 
 import (
+	"io"
+	"os"
+	"path"
 	"testing"
+
+	"github.com/marcelo-rocha/skaner/domain/checker"
+	"github.com/marcelo-rocha/skaner/domain/checker/sql"
+	"github.com/marcelo-rocha/skaner/domain/sourcecode"
+	"github.com/stretchr/testify/require"
 )
 
 func TestSQL(t *testing.T) {
+	testCases := []struct {
+		fileName string
+		lines    []int
+	}{
+		{fileName: "sql1.go", lines: []int{7}},
+		{fileName: "sql2.go", lines: []int{7, 15}},
+	}
 
+	workDir, _ := os.Getwd()
+	var sqlChecker checker.Checker = sql.New()
+
+	for i := range testCases {
+		require.True(t, sqlChecker.SupportedFileExtension(testCases[i].fileName))
+		srcFilePath := path.Join(workDir, "../../../test/data/", testCases[i].fileName)
+		f, err := os.Open(srcFilePath)
+		require.NoError(t, err)
+		defer f.Close()
+
+		content, err := io.ReadAll(f)
+		require.NoError(t, err)
+
+		src := sourcecode.NewSourceCode(content, testCases[i].fileName)
+
+		list, err := sqlChecker.Check(src)
+		require.NoError(t, err)
+
+		require.Len(t, list, len(testCases[i].lines))
+		for j := range list {
+			require.Equal(t, testCases[i].lines[j], list[j].Line)
+		}
+	}
 }
---
+++ test/data/sql2.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func GetCmd20(whereArg string) string {
+
+	return fmt.Sprintf("SELECT name FROM Clients WHERE %s", whereArg)
+}
+
+func GetCmd21(whereArg string) string {
+	return fmt.Sprintln("SELECT name FROM Clients WHERE s=1")
+}
+
+func GetCmd22(whereArg []byte) string {
+	return fmt.Sprintf("SELECT name FROM User WHERE id=%s", string(whereArg))
+}
